a28d902f249407dedc22bf423eb7362dae7582e1
reszkojr <s.reszkojr@gmail.com>
2021-11-12 10:04:10 -0300

    Main View controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,7 @@ module me.reszkojr.workshopjavafxjdbc {
 
     opens me.reszkojr.workshopjavafxjdbc to javafx.fxml;
     exports me.reszkojr.workshopjavafxjdbc;
+
+    opens me.reszkojr.workshopjavafxjdbc.controllers to javafx.fxml;
+    exports me.reszkojr.workshopjavafxjdbc.controllers;
 }
